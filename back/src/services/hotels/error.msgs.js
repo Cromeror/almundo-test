f0245c0d710266d47f329cfb94c83cb5c6396451
Cristobal Romero Rossi <cristobalromerorossi@gmail.com>
2018-06-04 01:33:12 -0500

    Added new validations in hotels service Validations in add new hotel

---
+++ back/src/services/hotels/error.msgs.js
@@ -0,0 +1,6 @@
+module.exports = {
+    bodyDataWithError: "Error in the structure of the hotel you are trying to add.",
+    priceRequired: "The price value is required.",
+    nameRequired: "The name is required.",
+    starsRequired: "You must indicate the number of stars."
+}
