9bb9f2bd738778c290dba8645b42abd86eb8d452
Cristobal Romero Rossi <cristobalromerorossi@gmail.com>
2018-06-05 06:23:23 -0500

    added services for get hotel data Now the service folder contains the functions to consume the rest services

---
+++ front/src/services/hotels.js
@@ -0,0 +1,8 @@
+import axios from "axios"
+import { apiUrl } from "../../config/localConfig"
+
+export default {
+    find: (query = "") => {
+        return axios.get(`${apiUrl}/hotels${query ? `?name=${query}` : ""}`)
+    }
+}
